eb6aa073f9eec50449188d530d578563d7501498
blambalem <test@test.com>
2026-08-09 23:10:39 +0300

    delete secrets

--- apps/app1/backend/main.py
+++ apps/app1/backend/main.py
@@ -102,5 +102,3 @@ def register_user(username: str = Form(...), password: str = Form(...)):
 def admin_panel():
     logger.error("SECURITY ALERT: Unauthorized access attempt to /admin-panel!")
     raise HTTPException(status_code=403, detail="Access denied. Incident logged.")
-
-DB_PASSWORD string = "Asjklndkmnvepoknasd"
